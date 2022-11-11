9bc29f8a72b6b6931289ad7703f45d5c0ae002da
Anderson Filho <andersonfilho.contato@outlook.com>
2022-11-11 16:00:23 -0300

    envio 3

---
+++ somaGauss.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void){
+    int n;
+    int r;
+    
+    
+    printf("Digite um numero > 1\n");
+    scanf("%d", &n);
+    if (n>1)
+    {
+        r = 1;
+        for ( int i = 1; i < n; i++)
+        {
+            r += i+1;
+
+        }
+        
+    }
+    
+        printf("%d\n",r);
+
+
+
+    return 0;
+}
