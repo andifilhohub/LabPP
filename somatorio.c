5bd39576b607b536e387b7dd5f8f0feeb83b18a2
andifilhohub <115162146+andifilhohub@users.noreply.github.com>
2022-12-04 23:26:23 -0300

    ENVIO 7

---
+++ somatorio.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+int div (y,x);
+int main(void){
+    int n;
+    float acum = 0.0;
+
+
+
+printf("Digite a quantiade de elementos\n");
+scanf("%d",&n);
+float vetor[n];
+for (int i = 0; i<n; i++){
+
+    printf("Informe o %d elemento: ", i);
+    scanf("%f", vetor+i);
+    }
+
+
+
+for (int i = 0; i < n; i++)
+{
+    if (i==(n-1))
+    {
+        printf("%g ", vetor[i]);
+        acum+=vetor[i];
+    }
+    else
+    {
+        printf("%g + ", vetor[i]);
+        acum+=vetor[i];
+    }
+}
+
+
+printf("= %g", acum);
+
+
+
+    return 0;
+}
